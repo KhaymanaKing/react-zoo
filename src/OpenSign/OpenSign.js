2e6cea8768fe0ac57c7ce5f255414881580ed7f0
Khayman King <khaymanak@gmail.com>
2022-04-28 17:52:01 -0700

    app finished mostly playing with css

--- src/OpenSign/OpenSign.js
+++ src/OpenSign/OpenSign.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Open.css';
 
 export default function OpenSign({ isOpen }) {
   return (
-    <div>
+    <div className= {isOpen ? 'open' : 'closed'}>
       {isOpen 
         ? 'Welcome to the Zoo!!!'
         : 'Get Out we are closed!'
