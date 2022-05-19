1873a7d8a15be55735f907efccf3c8ee748c1071
Khayman King <khaymanak@gmail.com>
2022-05-19 10:09:03 -0700

    set up router and cleaned up app.js css is a mess though

--- src/App.js
+++ src/App.js
@@ -2,63 +2,32 @@ import './App.css';
 import ParadeList from './Parade/ParadeList';
 import OpenSign from './OpenSign/OpenSign';
 import EatingCompetition from './EatingCompetition/EatingCompetition';
-import Button from '@mui/material/Button/Button';
+import Admin from './Admin';
 
-import { useState } from 'react';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+  Link,
+} from 'react-router-dom';
 function App() {
-  const [isOpen, setIsOpen] = useState(true);
-  const [animal, setAnimal] = useState (['giraffe', 'Dinosaur']);
-  
-  function handleOpen(){
-    setIsOpen(true);
-  }
-  function handleClose(){
-    setIsOpen(false);
-  }
-
-  function handleGiraffe(){
-    animal.push('giraffe');
-    setAnimal([...animal]);
-  }
-  function handleDinosaur(){
-    animal.push('dinosaur');
-    setAnimal([...animal]);
-  }
-  function handleTurtle(){
-    animal.push('turtle');
-    setAnimal([...animal]);
-  }
-  function handleSnake(){
-    animal.push('snake');
-    setAnimal([...animal]);
-  }
-  function handleDelete(){
-    animal.pop();
-    setAnimal([...animal]);
-  }
-  
-  
   return (
-    <div className="App">
-      <header className="App-header">
-        Khaymans Zoo!
-      </header>
-      
-      <OpenSign isOpen={isOpen}/>
-      <Button variant='contained'value='open' onClick={handleOpen}>Open?</Button>
-      <Button variant='contained' color='error' value='close' onClick={handleClose}>Close it Down!</Button>
-      <EatingCompetition/>
-      <h1> Worlds smallest Emoji Parade</h1>
-      <ParadeList animals ={animal}/>    
-      <div className='animal-buttons'>
-        <Button variant='contained' color='warning' onClick={handleGiraffe}>Giraffe</Button>
-        <Button variant='contained' color='inherit' onClick={handleDinosaur}>Dinosaur</Button>
-        <Button variant='contained' color='success' onClick={handleTurtle}> Turtle</Button>
-        <Button variant='contained' onClick={handleSnake}>Snake</Button>
-        <Button variant='contained' color='error' onClick={handleDelete}>Remove Last?</Button>
-      </div>
-    </div>
+    <Router>
+      <Route exact path ='/'>
+        <div className="App">
+          <header className="App-header">
+            <Link to='/admin'>Admin</Link>
+          </header>
+          <OpenSign />
+          <EatingCompetition />
+          <ParadeList />
+        </div>
+      </Route>
+      <Route exact path ='/admin'>
+        <Admin/>
+      </Route>
+    </Router>
   );
 }
 
--- src/OpenSign/OpenSign.js
+++ src/OpenSign/OpenSign.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Open.css';
+import Button from '@mui/material/Button/Button';
 
-export default function OpenSign({ isOpen }) {
+export default function OpenSign() {
+  const [isOpen, setIsOpen] = useState(true);
+  function handleOpen(){
+    setIsOpen(true);
+  }
+  function handleClose(){
+    setIsOpen(false);
+  }
+
   return (
     <div className= {isOpen ? 'open' : 'closed'}>
       {isOpen 
         ? 'Welcome to the Zoo!!!'
         : 'Get Out we are closed!'
       }
+      <Button variant='contained'value='open' onClick={handleOpen}>Open?</Button>
+      <Button variant='contained' color='error' value='close' onClick={handleClose}>Close it Down!</Button>
     </div>
   );
 }
