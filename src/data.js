cb9ea47f7b481bb4f93f0bb9330552293dfd93ef
Khayman King <khaymanak@gmail.com>
2022-05-19 10:18:22 -0700

    made data.js

---
+++ src/data.js
@@ -0,0 +1,802 @@
+export const data = [
+  {
+    'id': 1,
+    'first_name': 'Gwendolin',
+    'last_name': 'Grasha',
+    'email': 'ggrasha0@mail.ru',
+    'gender': 'Female',
+    'description': 'structure'
+  },
+  {
+    'id': 2,
+    'first_name': 'Jarvis',
+    'last_name': 'Kenwood',
+    'email': 'jkenwood1@who.int',
+    'gender': 'Male',
+    'description': 'Persevering'
+  },
+  {
+    'id': 3,
+    'first_name': 'Luci',
+    'last_name': 'Teasell',
+    'email': 'lteasell2@vk.com',
+    'gender': 'Female',
+    'description': 'User-centric'
+  },
+  {
+    'id': 4,
+    'first_name': 'Delila',
+    'last_name': 'Teideman',
+    'email': 'dteideman3@netlog.com',
+    'gender': 'Female',
+    'description': 'object-oriented'
+  },
+  {
+    'id': 5,
+    'first_name': 'Emera',
+    'last_name': 'Ivashnikov',
+    'email': 'eivashnikov4@csmonitor.com',
+    'gender': 'Female',
+    'description': 'synergy'
+  },
+  {
+    'id': 6,
+    'first_name': 'Kacey',
+    'last_name': 'Twinbourne',
+    'email': 'ktwinbourne5@i2i.jp',
+    'gender': 'Female',
+    'description': 'Face to face'
+  },
+  {
+    'id': 7,
+    'first_name': 'Alexandra',
+    'last_name': 'Stanesby',
+    'email': 'astanesby6@amazonaws.com',
+    'gender': 'Female',
+    'description': 'workforce'
+  },
+  {
+    'id': 8,
+    'first_name': 'Kelwin',
+    'last_name': 'Kitney',
+    'email': 'kkitney7@networksolutions.com',
+    'gender': 'Male',
+    'description': 'leverage'
+  },
+  {
+    'id': 9,
+    'first_name': 'Meir',
+    'last_name': 'Twelvetree',
+    'email': 'mtwelvetree8@dailymotion.com',
+    'gender': 'Male',
+    'description': 'software'
+  },
+  {
+    'id': 10,
+    'first_name': 'Justus',
+    'last_name': 'Rainbow',
+    'email': 'jrainbow9@adobe.com',
+    'gender': 'Male',
+    'description': 'Face to face'
+  },
+  {
+    'id': 11,
+    'first_name': 'Frannie',
+    'last_name': 'Foran',
+    'email': 'fforana@cpanel.net',
+    'gender': 'Male',
+    'description': 'Balanced'
+  },
+  {
+    'id': 12,
+    'first_name': 'Laurel',
+    'last_name': 'Chater',
+    'email': 'lchaterb@fda.gov',
+    'gender': 'Female',
+    'description': 'empowering'
+  },
+  {
+    'id': 13,
+    'first_name': 'Hilliard',
+    'last_name': 'MacGettigen',
+    'email': 'hmacgettigenc@netscape.com',
+    'gender': 'Male',
+    'description': '24 hour'
+  },
+  {
+    'id': 14,
+    'first_name': 'Leicester',
+    'last_name': 'Bassingham',
+    'email': 'lbassinghamd@bigcartel.com',
+    'gender': 'Male',
+    'description': 'Cross-group'
+  },
+  {
+    'id': 15,
+    'first_name': 'Tera',
+    'last_name': 'Wharrier',
+    'email': 'twharriere@nps.gov',
+    'gender': 'Female',
+    'description': 'Universal'
+  },
+  {
+    'id': 16,
+    'first_name': 'Curr',
+    'last_name': 'Poyner',
+    'email': 'cpoynerf@wikia.com',
+    'gender': 'Male',
+    'description': 'architecture'
+  },
+  {
+    'id': 17,
+    'first_name': 'Renard',
+    'last_name': 'Tongue',
+    'email': 'rtongueg@facebook.com',
+    'gender': 'Male',
+    'description': 'well-modulated'
+  },
+  {
+    'id': 18,
+    'first_name': 'Vale',
+    'last_name': 'Stoate',
+    'email': 'vstoateh@squarespace.com',
+    'gender': 'Bigender',
+    'description': 'Cross-platform'
+  },
+  {
+    'id': 19,
+    'first_name': 'Sergei',
+    'last_name': 'Cuttings',
+    'email': 'scuttingsi@imgur.com',
+    'gender': 'Male',
+    'description': 'Progressive'
+  },
+  {
+    'id': 20,
+    'first_name': 'Jefferey',
+    'last_name': 'Sendley',
+    'email': 'jsendleyj@irs.gov',
+    'gender': 'Genderqueer',
+    'description': 'transitional'
+  },
+  {
+    'id': 21,
+    'first_name': 'Erminie',
+    'last_name': 'Howlett',
+    'email': 'ehowlettk@discovery.com',
+    'gender': 'Female',
+    'description': 'monitoring'
+  },
+  {
+    'id': 22,
+    'first_name': 'Clarita',
+    'last_name': 'Lugg',
+    'email': 'cluggl@github.com',
+    'gender': 'Female',
+    'description': 'emulation'
+  },
+  {
+    'id': 23,
+    'first_name': 'Demetrius',
+    'last_name': 'Hebbron',
+    'email': 'dhebbronm@google.ca',
+    'gender': 'Male',
+    'description': 'well-modulated'
+  },
+  {
+    'id': 24,
+    'first_name': 'Farand',
+    'last_name': 'MacKnight',
+    'email': 'fmacknightn@jugem.jp',
+    'gender': 'Female',
+    'description': 'adapter'
+  },
+  {
+    'id': 25,
+    'first_name': 'Shawn',
+    'last_name': 'Mileham',
+    'email': 'smilehamo@yellowpages.com',
+    'gender': 'Male',
+    'description': 'Customer-focused'
+  },
+  {
+    'id': 26,
+    'first_name': 'Rocky',
+    'last_name': 'Leverentz',
+    'email': 'rleverentzp@a8.net',
+    'gender': 'Male',
+    'description': 'eco-centric'
+  },
+  {
+    'id': 27,
+    'first_name': 'Margi',
+    'last_name': 'Wetherby',
+    'email': 'mwetherbyq@netvibes.com',
+    'gender': 'Genderqueer',
+    'description': 'Pre-emptive'
+  },
+  {
+    'id': 28,
+    'first_name': 'Gordie',
+    'last_name': 'Ravenshear',
+    'email': 'gravenshearr@com.com',
+    'gender': 'Male',
+    'description': 'Universal'
+  },
+  {
+    'id': 29,
+    'first_name': 'Goober',
+    'last_name': 'Domm',
+    'email': 'gdomms@scientificamerican.com',
+    'gender': 'Male',
+    'description': 'Pre-emptive'
+  },
+  {
+    'id': 30,
+    'first_name': 'Alfi',
+    'last_name': "O'Sheeryne",
+    'email': 'aosheerynet@microsoft.com',
+    'gender': 'Female',
+    'description': 'Synergistic'
+  },
+  {
+    'id': 31,
+    'first_name': 'Angele',
+    'last_name': 'McGlade',
+    'email': 'amcgladeu@dot.gov',
+    'gender': 'Non-binary',
+    'description': 'Reactive'
+  },
+  {
+    'id': 32,
+    'first_name': 'Eliot',
+    'last_name': 'Collister',
+    'email': 'ecollisterv@surveymonkey.com',
+    'gender': 'Genderfluid',
+    'description': 'uniform'
+  },
+  {
+    'id': 33,
+    'first_name': 'Gabbi',
+    'last_name': 'Edeler',
+    'email': 'gedelerw@e-recht24.de',
+    'gender': 'Non-binary',
+    'description': 'Inverse'
+  },
+  {
+    'id': 34,
+    'first_name': 'Dino',
+    'last_name': 'Addlestone',
+    'email': 'daddlestonex@mediafire.com',
+    'gender': 'Male',
+    'description': 'eco-centric'
+  },
+  {
+    'id': 35,
+    'first_name': 'Phaidra',
+    'last_name': 'Winward',
+    'email': 'pwinwardy@businessinsider.com',
+    'gender': 'Female',
+    'description': 'matrices'
+  },
+  {
+    'id': 36,
+    'first_name': 'Ryan',
+    'last_name': 'Harrald',
+    'email': 'rharraldz@msn.com',
+    'gender': 'Male',
+    'description': 'parallelism'
+  },
+  {
+    'id': 37,
+    'first_name': 'Marysa',
+    'last_name': 'Yare',
+    'email': 'myare10@abc.net.au',
+    'gender': 'Female',
+    'description': 'contingency'
+  },
+  {
+    'id': 38,
+    'first_name': 'Georgeanna',
+    'last_name': 'Crinion',
+    'email': 'gcrinion11@upenn.edu',
+    'gender': 'Female',
+    'description': 'multi-tasking'
+  },
+  {
+    'id': 39,
+    'first_name': 'Pieter',
+    'last_name': 'Abreheart',
+    'email': 'pabreheart12@eepurl.com',
+    'gender': 'Male',
+    'description': '6th generation'
+  },
+  {
+    'id': 40,
+    'first_name': 'Saundra',
+    'last_name': 'Dorman',
+    'email': 'sdorman13@themeforest.net',
+    'gender': 'Female',
+    'description': 'local'
+  },
+  {
+    'id': 41,
+    'first_name': 'Modesta',
+    'last_name': 'Rawlinson',
+    'email': 'mrawlinson14@umn.edu',
+    'gender': 'Female',
+    'description': 'homogeneous'
+  },
+  {
+    'id': 42,
+    'first_name': 'Denyse',
+    'last_name': 'Brigg',
+    'email': 'dbrigg15@nasa.gov',
+    'gender': 'Female',
+    'description': 'Virtual'
+  },
+  {
+    'id': 43,
+    'first_name': 'Constance',
+    'last_name': 'Camies',
+    'email': 'ccamies16@vistaprint.com',
+    'gender': 'Female',
+    'description': 'Integrated'
+  },
+  {
+    'id': 44,
+    'first_name': 'Wain',
+    'last_name': 'Winspear',
+    'email': 'wwinspear17@businessweek.com',
+    'gender': 'Agender',
+    'description': 'software'
+  },
+  {
+    'id': 45,
+    'first_name': 'Novelia',
+    'last_name': 'Gylle',
+    'email': 'ngylle18@example.com',
+    'gender': 'Female',
+    'description': 'Profound'
+  },
+  {
+    'id': 46,
+    'first_name': 'Laurie',
+    'last_name': 'Tick',
+    'email': 'ltick19@qq.com',
+    'gender': 'Male',
+    'description': 'frame'
+  },
+  {
+    'id': 47,
+    'first_name': 'Galvan',
+    'last_name': 'Pawels',
+    'email': 'gpawels1a@tamu.edu',
+    'gender': 'Male',
+    'description': 'mobile'
+  },
+  {
+    'id': 48,
+    'first_name': 'Emalia',
+    'last_name': 'Roth',
+    'email': 'eroth1b@ycombinator.com',
+    'gender': 'Female',
+    'description': 'fresh-thinking'
+  },
+  {
+    'id': 49,
+    'first_name': 'Sigfried',
+    'last_name': 'Vignaux',
+    'email': 'svignaux1c@blinklist.com',
+    'gender': 'Male',
+    'description': 'Multi-layered'
+  },
+  {
+    'id': 50,
+    'first_name': 'Felice',
+    'last_name': 'Cakebread',
+    'email': 'fcakebread1d@ftc.gov',
+    'gender': 'Male',
+    'description': 'Graphic Interface'
+  },
+  {
+    'id': 51,
+    'first_name': 'Brynn',
+    'last_name': 'Feild',
+    'email': 'bfeild1e@thetimes.co.uk',
+    'gender': 'Female',
+    'description': '5th generation'
+  },
+  {
+    'id': 52,
+    'first_name': 'Aksel',
+    'last_name': 'Dignan',
+    'email': 'adignan1f@quantcast.com',
+    'gender': 'Male',
+    'description': 'Profit-focused'
+  },
+  {
+    'id': 53,
+    'first_name': 'Baillie',
+    'last_name': 'Conew',
+    'email': 'bconew1g@bravesites.com',
+    'gender': 'Male',
+    'description': 'Up-sized'
+  },
+  {
+    'id': 54,
+    'first_name': 'Elysee',
+    'last_name': 'Ruddle',
+    'email': 'eruddle1h@wp.com',
+    'gender': 'Female',
+    'description': 'systematic'
+  },
+  {
+    'id': 55,
+    'first_name': 'Shirl',
+    'last_name': 'Delaprelle',
+    'email': 'sdelaprelle1i@addthis.com',
+    'gender': 'Non-binary',
+    'description': 'web-enabled'
+  },
+  {
+    'id': 56,
+    'first_name': 'Shelton',
+    'last_name': 'Bernardinelli',
+    'email': 'sbernardinelli1j@phoca.cz',
+    'gender': 'Male',
+    'description': 'mission-critical'
+  },
+  {
+    'id': 57,
+    'first_name': 'Kasey',
+    'last_name': 'Randerson',
+    'email': 'kranderson1k@washington.edu',
+    'gender': 'Female',
+    'description': 'Virtual'
+  },
+  {
+    'id': 58,
+    'first_name': 'Elsa',
+    'last_name': 'Shand',
+    'email': 'eshand1l@trellian.com',
+    'gender': 'Female',
+    'description': 'local area network'
+  },
+  {
+    'id': 59,
+    'first_name': 'Bogart',
+    'last_name': 'Wallbank',
+    'email': 'bwallbank1m@unicef.org',
+    'gender': 'Male',
+    'description': 'definition'
+  },
+  {
+    'id': 60,
+    'first_name': 'Isidro',
+    'last_name': 'Gauford',
+    'email': 'igauford1n@paypal.com',
+    'gender': 'Male',
+    'description': 'Object-based'
+  },
+  {
+    'id': 61,
+    'first_name': 'Kristofer',
+    'last_name': 'Purcell',
+    'email': 'kpurcell1o@networksolutions.com',
+    'gender': 'Male',
+    'description': 'Networked'
+  },
+  {
+    'id': 62,
+    'first_name': 'Giulio',
+    'last_name': 'Spearing',
+    'email': 'gspearing1p@dmoz.org',
+    'gender': 'Male',
+    'description': 'Multi-layered'
+  },
+  {
+    'id': 63,
+    'first_name': 'Anthe',
+    'last_name': 'Shearmer',
+    'email': 'ashearmer1q@tuttocitta.it',
+    'gender': 'Female',
+    'description': 'matrix'
+  },
+  {
+    'id': 64,
+    'first_name': 'Wanids',
+    'last_name': 'Eiler',
+    'email': 'weiler1r@scribd.com',
+    'gender': 'Bigender',
+    'description': 'encoding'
+  },
+  {
+    'id': 65,
+    'first_name': 'Vince',
+    'last_name': 'Filde',
+    'email': 'vfilde1s@biblegateway.com',
+    'gender': 'Male',
+    'description': 'next generation'
+  },
+  {
+    'id': 66,
+    'first_name': 'Theo',
+    'last_name': 'Rillatt',
+    'email': 'trillatt1t@bbc.co.uk',
+    'gender': 'Polygender',
+    'description': 'Monitored'
+  },
+  {
+    'id': 67,
+    'first_name': 'Violetta',
+    'last_name': 'Pipes',
+    'email': 'vpipes1u@qq.com',
+    'gender': 'Female',
+    'description': 'content-based'
+  },
+  {
+    'id': 68,
+    'first_name': 'Goldarina',
+    'last_name': 'Howroyd',
+    'email': 'ghowroyd1v@4shared.com',
+    'gender': 'Female',
+    'description': 'monitoring'
+  },
+  {
+    'id': 69,
+    'first_name': 'Gaile',
+    'last_name': 'Steuhlmeyer',
+    'email': 'gsteuhlmeyer1w@loc.gov',
+    'gender': 'Male',
+    'description': 'attitude-oriented'
+  },
+  {
+    'id': 70,
+    'first_name': 'Martie',
+    'last_name': 'Punshon',
+    'email': 'mpunshon1x@uiuc.edu',
+    'gender': 'Male',
+    'description': 'Fundamental'
+  },
+  {
+    'id': 71,
+    'first_name': 'Marrilee',
+    'last_name': 'Berringer',
+    'email': 'mberringer1y@istockphoto.com',
+    'gender': 'Non-binary',
+    'description': 'Intuitive'
+  },
+  {
+    'id': 72,
+    'first_name': 'Griffy',
+    'last_name': 'Nertney',
+    'email': 'gnertney1z@scribd.com',
+    'gender': 'Male',
+    'description': 'attitude-oriented'
+  },
+  {
+    'id': 73,
+    'first_name': 'Alvin',
+    'last_name': 'Ollin',
+    'email': 'aollin20@diigo.com',
+    'gender': 'Genderfluid',
+    'description': 'knowledge base'
+  },
+  {
+    'id': 74,
+    'first_name': 'Torie',
+    'last_name': 'Bacup',
+    'email': 'tbacup21@google.ca',
+    'gender': 'Female',
+    'description': 'project'
+  },
+  {
+    'id': 75,
+    'first_name': 'Gaspard',
+    'last_name': 'Dowsey',
+    'email': 'gdowsey22@huffingtonpost.com',
+    'gender': 'Male',
+    'description': 'attitude-oriented'
+  },
+  {
+    'id': 76,
+    'first_name': 'Jerrylee',
+    'last_name': 'Glawsop',
+    'email': 'jglawsop23@ezinearticles.com',
+    'gender': 'Female',
+    'description': 'circuit'
+  },
+  {
+    'id': 77,
+    'first_name': 'Rudiger',
+    'last_name': 'Boole',
+    'email': 'rboole24@wikipedia.org',
+    'gender': 'Male',
+    'description': 'Upgradable'
+  },
+  {
+    'id': 78,
+    'first_name': 'Freddie',
+    'last_name': 'Pawden',
+    'email': 'fpawden25@rambler.ru',
+    'gender': 'Genderqueer',
+    'description': 'challenge'
+  },
+  {
+    'id': 79,
+    'first_name': 'Jo ann',
+    'last_name': 'Guye',
+    'email': 'jguye26@altervista.org',
+    'gender': 'Female',
+    'description': 'optimizing'
+  },
+  {
+    'id': 80,
+    'first_name': 'Jacky',
+    'last_name': 'Edlyn',
+    'email': 'jedlyn27@unc.edu',
+    'gender': 'Female',
+    'description': '5th generation'
+  },
+  {
+    'id': 81,
+    'first_name': 'Mei',
+    'last_name': 'Albisser',
+    'email': 'malbisser28@hexun.com',
+    'gender': 'Female',
+    'description': 'process improvement'
+  },
+  {
+    'id': 82,
+    'first_name': 'Larina',
+    'last_name': 'Costley',
+    'email': 'lcostley29@rediff.com',
+    'gender': 'Female',
+    'description': 'optimal'
+  },
+  {
+    'id': 83,
+    'first_name': 'Darrick',
+    'last_name': 'Ladel',
+    'email': 'dladel2a@huffingtonpost.com',
+    'gender': 'Male',
+    'description': 'capability'
+  },
+  {
+    'id': 84,
+    'first_name': 'Steffane',
+    'last_name': 'Bowley',
+    'email': 'sbowley2b@utexas.edu',
+    'gender': 'Non-binary',
+    'description': 'strategy'
+  },
+  {
+    'id': 85,
+    'first_name': 'Monah',
+    'last_name': 'Allbrook',
+    'email': 'mallbrook2c@naver.com',
+    'gender': 'Agender',
+    'description': 'parallelism'
+  },
+  {
+    'id': 86,
+    'first_name': 'Jane',
+    'last_name': 'Gaunt',
+    'email': 'jgaunt2d@drupal.org',
+    'gender': 'Female',
+    'description': 'model'
+  },
+  {
+    'id': 87,
+    'first_name': 'Waldon',
+    'last_name': 'Rottcher',
+    'email': 'wrottcher2e@xing.com',
+    'gender': 'Male',
+    'description': 'attitude-oriented'
+  },
+  {
+    'id': 88,
+    'first_name': 'Raimundo',
+    'last_name': 'Halfhide',
+    'email': 'rhalfhide2f@bigcartel.com',
+    'gender': 'Male',
+    'description': 'Implemented'
+  },
+  {
+    'id': 89,
+    'first_name': 'Winthrop',
+    'last_name': 'Boulsher',
+    'email': 'wboulsher2g@1und1.de',
+    'gender': 'Male',
+    'description': 'analyzing'
+  },
+  {
+    'id': 90,
+    'first_name': 'Fay',
+    'last_name': 'Harvie',
+    'email': 'fharvie2h@intel.com',
+    'gender': 'Female',
+    'description': 'Realigned'
+  },
+  {
+    'id': 91,
+    'first_name': 'Berta',
+    'last_name': 'Blackmore',
+    'email': 'bblackmore2i@senate.gov',
+    'gender': 'Polygender',
+    'description': 'matrix'
+  },
+  {
+    'id': 92,
+    'first_name': 'Kerry',
+    'last_name': 'Felgat',
+    'email': 'kfelgat2j@loc.gov',
+    'gender': 'Male',
+    'description': 'structure'
+  },
+  {
+    'id': 93,
+    'first_name': 'Dorri',
+    'last_name': 'Adrienne',
+    'email': 'dadrienne2k@nydailynews.com',
+    'gender': 'Female',
+    'description': 'data-warehouse'
+  },
+  {
+    'id': 94,
+    'first_name': 'Sharron',
+    'last_name': 'Matussov',
+    'email': 'smatussov2l@gnu.org',
+    'gender': 'Female',
+    'description': 'directional'
+  },
+  {
+    'id': 95,
+    'first_name': 'Cammy',
+    'last_name': 'Caldeyroux',
+    'email': 'ccaldeyroux2m@usatoday.com',
+    'gender': 'Female',
+    'description': 'Enhanced'
+  },
+  {
+    'id': 96,
+    'first_name': 'Hort',
+    'last_name': 'Emerine',
+    'email': 'hemerine2n@noaa.gov',
+    'gender': 'Genderfluid',
+    'description': 'database'
+  },
+  {
+    'id': 97,
+    'first_name': 'Abbey',
+    'last_name': 'Nathan',
+    'email': 'anathan2o@nbcnews.com',
+    'gender': 'Male',
+    'description': 'local'
+  },
+  {
+    'id': 98,
+    'first_name': 'Benni',
+    'last_name': 'Coll',
+    'email': 'bcoll2p@hud.gov',
+    'gender': 'Female',
+    'description': 'mobile'
+  },
+  {
+    'id': 99,
+    'first_name': 'Kaylil',
+    'last_name': 'Venes',
+    'email': 'kvenes2q@elegantthemes.com',
+    'gender': 'Female',
+    'description': 'Focused'
+  },
+  {
+    'id': 100,
+    'first_name': 'Melvin',
+    'last_name': 'Ferryman',
+    'email': 'mferryman2r@imageshack.us',
+    'gender': 'Male',
+    'description': 'Open-source'
+  }
+];
