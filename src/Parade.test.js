25de2784383c8a4c5945af66a740d6973cfe1978
Khayman King <khaymanak@gmail.com>
2022-05-18 17:53:22 -0700

    parade test finished

--- src/Parade.test.js
+++ src/Parade.test.js
@@ -12,3 +12,15 @@ test('when animal is click it should appear on screen', () => {
   expect(endEmoji.length).toBe(2);
 
 });
+test('when animal is click it should appear on screen', () => {
+  render(<App/>);
+  const startEmoji = screen.queryAllByText(/🦒/i);
+  expect(startEmoji.length).toBe(1);
+  const addGiraffe = screen.getByText(/giraffe/i);
+  fireEvent.click(addGiraffe);
+  const removeAnimal = screen.getByText(/Remove Last?/i);
+  fireEvent.click(removeAnimal);
+  const endEmoji = screen.queryAllByText(/🦒/i);
+  expect(endEmoji.length).toBe(1);
+  
+});
