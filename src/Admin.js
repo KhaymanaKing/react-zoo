812926ae893c3258954729c9f09ed170cb487311
Khayman King <khaymanak@gmail.com>
2022-05-20 10:10:51 -0700

    app should be finished

--- src/Admin.js
+++ src/Admin.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import MyCharts from './MyCharts';
+import MyTables from './MyTables';
 
 export default function Admin() {
   return (
     <div>
+      <MyTables/>
       <MyCharts/>
     </div>
   );
--- src/MyTables.js
+++ src/MyTables.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import MaterialTable from 'material-table';
+import { customers } from './data.js';
 
 export default function MyTables() {
   return (
-    <div>MyTables</div>
+    <div style={{ maxWidth: '100%' }}>
+      <MaterialTable 
+        columns={
+          [
+            { title: 'ID', field: 'id' },
+            { title: 'First Name', field: 'first_name' },
+            { title: 'Last Name', field: 'last_name' },
+            { title: 'Email', field: 'email' },
+            { title: 'Gender', field: 'gender' },
+            { title: 'Description', field: 'description' },
+          ]
+        }
+        data={customers}
+        title='List of Suspects'
+        localization={{
+          pagination : {
+            nextAriaLabel: 'next',
+            previousAriaLabel: 'previous'
+          }
+        }}/>
+    </div>
   );
 }
